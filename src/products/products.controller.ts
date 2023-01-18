3f375009bccdb721bdc7d09bce807750c92edc0b
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-18 15:27:39 -0300

    crud logic

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,17 +16,18 @@ import { ProductsService } from './products.service';
 // import { CreateProductDto } from './dto/create-product.dto';
 // import { UpdateProductDto } from './dto/update-product.dto';
 import { Response } from 'express';
+
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get('')
   getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string
+    // @Query('limit') limit = 100,
+    // @Query('offset') offset = 0,
+    // @Query('brand') brand: string
   ) {
-    return {message :`products limit=> ${limit} offset=> ${offset} brand=> ${brand}`};
+    return this.productsService.findAll()
   }
 
   @Get('filter')
@@ -36,30 +37,23 @@ export class ProductsController {
 
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
-  getOne( @Res() response: Response , @Param('productId') productId: string) {
-    return {
-     message: `product ${productId}`,
-  };
+  getOne( @Res() response: Response , @Param('id') id: number) {
+    return this.productsService.findOne(id)
+
 }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'create action',
-      payload,
-    };
+    return this.productsService.create(payload)
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload
-    };
+    return this.productsService.update(id, payload)
   }
 
   @Delete(':id')
   delete(@Param('id') id: number,) {
-    return { id }
+    return this.productsService.delete(id)
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { Product } from './entities/product.entity';
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'garloop',
+      price: 100,
+      image: '',
+      stock: 100,
+    },
+  ];
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+
+    const NewProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(NewProduct);
+    return NewProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.products.find((item) => item.id == id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: number, payload: any) {
+    const productFound = this.products.findIndex((item) => item.id === id);
+    let message = '';
+    if (productFound > 0) {
+      this.products[productFound] = {
+        id: id,
+        ...payload,
+      };
+      message = 'Product updated';
+    } else {
+      message = 'Product not found';
+    }
+    return message;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  delete(id: number) {
+    const productFound = this.products.findIndex((item) => item.id === id);
+    let message = '';
+    if (productFound > 0) {
+      this.products.splice(productFound, 1);
+      message = 'product deleted';
+    } else {
+      message = 'product not found';
+    }
+    return message;
   }
 }
