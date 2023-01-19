eb8babbd4217cd84a019a5688cc149495eb44ccb
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-19 12:40:36 -0300

    fixing updatedtousers

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,5 +2,10 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-
+  readonly id?: number;
+  readonly name?: string;
+  readonly email?: string;
+  readonly password?: string;
+  readonly address?: string;
+  readonly image?: string;
 }
